fff80d75ac72854a2957a05591bce5a375941df2
tawan <87tawanzinho@gmail.com>
2023-12-24 03:57:10 -0300

    training button and viewBinding

--- app/src/main/java/com/example/gasto/MainActivity.kt
+++ app/src/main/java/com/example/gasto/MainActivity.kt
@@ -2,14 +2,28 @@ package com.example.gasto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.gasto.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding.buttonSend.setOnClickListener(this)
+    }
 
+    override fun onClick(view: View) {
+    if(view.id == R.id.button_send) {
+        calculate()
+        }
+    }
+
+    private fun calculate () {
+        Toast.makeText(this, "Fui clicado", Toast.LENGTH_LONG).show()
     }
 }
